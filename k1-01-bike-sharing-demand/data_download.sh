8f15dad7e649417626e36908278f20bba50a2455
KYUNG MO KWEON <kkweon@gmail.com>
2017-04-08 13:05:20 -0700

    fix: data download script
    1. Use pushd, popd. After downloading, it will go back to the original directory
    2. Remove a dependency on lxml for Beautifulsoup
    3. Add a kaggle.ini check in kaggle_submit.py

--- k1-01-bike-sharing-demand/data_download.sh
+++ k1-01-bike-sharing-demand/data_download.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 competition="bike-sharing-demand"
 curr_dir=${PWD##*/}  
+pushd $PWD
 echo "Downlaoding $competition to $curr_dir ..."
 cd ..; python3 kaggle_download.py $competition --destination $curr_dir
+popd
