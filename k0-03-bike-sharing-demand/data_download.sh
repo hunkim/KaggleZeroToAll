2dd6ac089b07c471bc8038cf2daf30deded2f4ec
Sung Kim <hunkim@gmail.com>
2017-04-08 21:55:11 +0800

    changed it to level 1

--- k0-03-bike-sharing-demand/data_download.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-competition="bike-sharing-demand"
-curr_dir=${PWD##*/}  
-echo "Downlaoding $competition to $curr_dir ..."
-cd ..; python3 kaggle_download.py $competition --destination $curr_dir
