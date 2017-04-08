7baba31c887a6a1c8ab5dac1ad2ff4fc8d481ea1
Sung Kim <hunkim@gmail.com>
2017-04-08 21:51:32 +0800

    Added bike sharing

---
+++ k0-03-bike-sharing-demand/data_download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+competition="bike-sharing-demand"
+curr_dir=${PWD##*/}  
+echo "Downlaoding $competition to $curr_dir ..."
+cd ..; python3 kaggle_download.py $competition --destination $curr_dir
